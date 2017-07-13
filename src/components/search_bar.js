ca0fe7d15114f54b8e1857b90006bae832c988b0
taewoong <moondingie5@gmail.com>
2017-07-14 02:17:29 +0900

    fetch youtube data

---
+++ src/components/search_bar.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+};
+
+const defaultProps = {
+};
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>SearchBar</div>
+    );
+  }
+}
+SearchBar.propTypes = propTypes;
+SearchBar.defaultProps = defaultProps;
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import API_KEY from '../config';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: [],
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <App />,
+  document.querySelector('.container')
+);
