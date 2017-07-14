a8de2bc2f888d13933a4d248989025916ee45dd6
taewoong <moondingie5@gmail.com>
2017-07-14 12:18:30 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class App extends Component {
     }
 
     YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-      this.setState({ videos })
+      this.setState({ videos });
     })
   }
 
